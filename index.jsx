ecf07d42700bf31e1cbfb4423bc29cb9733bde0b
Sampurna Biswas <156777916+SamPurna023@users.noreply.github.com>
2024-08-31 03:54:21 +0530

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,280 @@
+import React from "react";
+import "./style.css";
+
+export const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <div className="div">
+        <div className="container">
+          <div className="trusted-by">About us</div>
+          <p className="occaecat-est-ipsum">
+            Scholar Quest is designed to simplify the process of finding scholarships. Our app provides up-to-date
+            information and personalized scholarship recommendations based on criteria such as location, field of study,
+            and financial need. Students can also manage applications and track deadlines all in one place, saving them
+            time and effort.
+          </p>
+        </div>
+        <div className="container-2">
+          <div className="overlap-group">
+            <div className="rectangle" />
+            <img className="image" alt="Image" src="image-19.png" />
+          </div>
+          <p className="aliqua-cupidatat-id">
+            &#34;Aliqua cupidatat id duis irure sunt exercitation voluptate cillum. Consectetur ad ex do in
+            reprehenderit est dolor elit et exercitation do ad. Consectetur ad ex do in reprehenderit est dolor elit et
+            exercitation&#34;
+          </p>
+          <div className="text-wrapper">Full name</div>
+          <div className="text-wrapper-2">Job title</div>
+          <img className="rating" alt="Rating" src="rating-1.svg" />
+          <button className="button">
+            <div className="text">{""}</div>
+            <img className="img" alt="Chevron right large" src="chevron-right-large.svg" />
+          </button>
+          <button className="button-2">
+            <div className="text-2">{""}</div>
+            <img className="img" alt="Chevron left large" src="chevron-left-large.svg" />
+          </button>
+          <div className="rectangle-2" />
+          <div className="rectangle-3" />
+          <div className="rectangle-4" />
+          <div className="rectangle-5" />
+          <div className="rectangle-6" />
+        </div>
+        <div className="overlap">
+          <div className="overlap-2">
+            <div className="group">
+              <div className="container-3">
+                <p className="p">Scholarships&nbsp;&nbsp;provided by Private or Government Organizations</p>
+                <button className="div-wrapper">
+                  <div className="text-wrapper-3">Learn more</div>
+                </button>
+                <div className="feature">Organization Scholarships</div>
+                <img className="image-2" alt="Image" src="image-20.png" />
+              </div>
+              <div className="container-4">
+                <p className="p">Scholarships provided by different Colleges or Universities</p>
+                <button className="div-wrapper">
+                  <div className="text-wrapper-3">Learn more</div>
+                </button>
+                <div className="feature">Institute Scholarships</div>
+                <img className="image-2" alt="Image" src="image-21.png" />
+              </div>
+              <div className="container-5">
+                <p className="p">Courses offered by different Institute</p>
+                <button className="div-wrapper">
+                  <div className="text-wrapper-3">Learn more</div>
+                </button>
+                <div className="feature">Course</div>
+                <img className="image-2" alt="Image" src="image-22.png" />
+              </div>
+            </div>
+            <div className="text-wrapper-4">Product Features</div>
+            <div className="frame">
+              <img
+                className="scholarship"
+                alt="Scholarship"
+                src="scholarship-application-form-foundation-concept-1.png"
+              />
+            </div>
+            <button className="button-3">
+              <div className="text-wrapper-5">Join us now</div>
+            </button>
+            <button className="button-4">
+              <div className="text-wrapper-6">Learn more</div>
+            </button>
+            <div className="website-heading-goes">Scholarships Made Simple.</div>
+            <p className="text-wrapper-7">
+              Occaecat est ipsum reprehenderit reprehenderit veniam anim laborum est esse duis occaecat reprehenderit
+              pariatur.
+            </p>
+            <div className="group-2">
+              <img className="scholarship-2" alt="Scholarship" src="scholarship.png" />
+              <div className="text-wrapper-8">Scholar Quest</div>
+            </div>
+          </div>
+          <div className="header-menu">
+            <div className="frame-2">
+              <div className="text-wrapper-9">Features</div>
+              <img className="chevron-down-large" alt="Chevron down large" src="chevron-down-large.svg" />
+            </div>
+            <div className="frame-3">
+              <div className="text-wrapper-9">Pricing</div>
+            </div>
+            <div className="frame-4">
+              <div className="text-wrapper-9">About us</div>
+            </div>
+            <div className="frame-5">
+              <div className="text-wrapper-9">Blog</div>
+            </div>
+          </div>
+          <div className="text-wrapper-10">Sign up</div>
+          <button className="button-5">
+            <div className="text-wrapper-11">Sign in</div>
+          </button>
+        </div>
+        <div className="container-6">
+          <div className="text-wrapper-12">Our Team</div>
+          <div className="container-7">
+            <div className="overlap-group-2">
+              <div className="professional-title">Team Member</div>
+              <div className="commodo-qui-nulla">Guwahati,Assam</div>
+            </div>
+            <div className="full-name">Pragyan Thapa</div>
+            <img className="logo-linkedin" alt="Logo linkedin" src="logo-linkedin-5.svg" />
+            <img className="logo-twitter" alt="Logo twitter" src="logo-twitter-5.svg" />
+            <img className="img-2" alt="Thapa" src="thapa-1.png" />
+          </div>
+          <div className="container-8">
+            <div className="professional-title-2">Founder</div>
+            <div className="full-name-2">Anushuya Paul</div>
+            <img className="logo-linkedin-2" alt="Logo linkedin" src="logo-linkedin-5.svg" />
+            <img className="logo-twitter-2" alt="Logo twitter" src="logo-twitter-5.svg" />
+            <div className="commodo-qui-nulla-2">Guwahati,Assam</div>
+            <img
+              className="whatsapp-image"
+              alt="Whatsapp image"
+              src="whatsapp-image-2024-08-30-at-21-36-46-6723b484.png"
+            />
+          </div>
+          <div className="container-9">
+            <div className="overlap-3">
+              <div className="professional-title-3">Team Member</div>
+              <div className="commodo-qui-nulla-3">Guwahati,Assam</div>
+            </div>
+            <div className="full-name-3">Sampurna Biswas</div>
+            <img className="logo-linkedin-3" alt="Logo linkedin" src="logo-linkedin-5.svg" />
+            <img className="logo-twitter-3" alt="Logo twitter" src="logo-twitter-5.svg" />
+            <img className="sampurna" alt="Sampurna" src="sampurna-1.png" />
+          </div>
+          <div className="overlap-4">
+            <div className="container-10">
+              <div className="overlap-5">
+                <div className="professional-title-4">Team Leader</div>
+                <div className="text-wrapper-13">Guwahati,Assam</div>
+              </div>
+              <div className="full-name-4">Anwesha Choudhury</div>
+              <img className="logo-linkedin-4" alt="Logo linkedin" src="logo-linkedin-5.svg" />
+              <img className="logo-twitter-4" alt="Logo twitter" src="logo-twitter-5.svg" />
+              <img className="image-3" alt="Image" src="image-26.png" />
+            </div>
+            <img className="img-2" alt="Anwesha" src="anwesha-1.png" />
+          </div>
+          <div className="overlap-6">
+            <div className="container-11">
+              <div className="professional-title-5">Team Member</div>
+              <div className="full-name-5">Samim Rezvi</div>
+              <img className="logo-linkedin-2" alt="Logo linkedin" src="logo-linkedin-5.svg" />
+              <img className="logo-twitter-5" alt="Logo twitter" src="logo-twitter-5.svg" />
+              <div className="commodo-qui-nulla-4">Guwahati,Assam</div>
+            </div>
+            <img className="samim" alt="Samim" src="samim-1.png" />
+          </div>
+          <button className="button-6">
+            <div className="text-2">{""}</div>
+            <img className="img" alt="Chevron right large" src="chevron-right-large.svg" />
+          </button>
+          <button className="button-7">
+            <div className="text-2">{""}</div>
+            <img className="img" alt="Chevron left large" src="chevron-left-large.svg" />
+          </button>
+          <div className="rectangle-7" />
+          <div className="rectangle-8" />
+          <div className="rectangle-9" />
+          <div className="rectangle-10" />
+          <div className="rectangle-11" />
+        </div>
+        <div className="container-12">
+          <div className="text-wrapper-14">Pricing</div>
+          <div className="overlap-7">
+            <p className="text-wrapper-15">Officia exercitation quis voluptate elit consequat nostrud</p>
+            <p className="simple-plans-for">
+              <span className="span">Simple plans </span>
+              <span className="text-wrapper-16">for everyone</span>
+            </p>
+          </div>
+          <div className="container-13">
+            <div className="give-it-a-try">GIVE IT A TRY</div>
+            <div className="text-wrapper-17">Free</div>
+            <p className="text-wrapper-18">Lorem est consectetur mollit repre</p>
+            <p className="text-wrapper-19">Ad aute occaecat duis i</p>
+            <img className="c-check" alt="C check" src="c-check-14.svg" />
+            <div className="text-wrapper-20">Dolor deserunt in no</div>
+            <p className="text-wrapper-21">Culpa do id eu sunt esse culpa officia</p>
+            <img className="img-3" alt="E remove" src="e-remove-4.svg" />
+            <button className="button-8">
+              <div className="text-wrapper-22">Get started</div>
+            </button>
+            <img className="img-4" alt="E remove" src="e-remove-4.svg" />
+            <img className="image-4" alt="Image" src="image-45.svg" />
+          </div>
+          <div className="container-14">
+            <div className="go-limitless">GO LIMITLESS</div>
+            <div className="text-wrapper-23">$12</div>
+            <p className="text-wrapper-24">Lorem est consectetur mollit repre</p>
+            <p className="text-wrapper-25">Ad aute occaecat duis i</p>
+            <img className="c-check" alt="C check" src="c-check-14.svg" />
+            <div className="text-wrapper-26">Dolor deserunt in no</div>
+            <p className="text-wrapper-27">Eu ea Lorem id nisi est officia cupidat</p>
+            <img className="img-3" alt="C check" src="c-check-14.svg" />
+            <button className="button-9">
+              <div className="text-wrapper-28">Get started</div>
+            </button>
+            <img className="img-4" alt="C check" src="c-check-14.svg" />
+            <img className="image-4" alt="Image" src="image-48.svg" />
+            <div className="lifetime-license">Premium</div>
+          </div>
+        </div>
+        <div className="container-15">
+          <img className="oval" alt="Oval" src="oval-1.svg" />
+          <img className="get-started" alt="Get started" src="get-started.png" />
+          <img
+            className="qui-ut-exercitation"
+            alt="Qui ut exercitation"
+            src="qui-ut-exercitation-officia-proident-enim-non-tempor-tempor-ipsum-ex-nulla-ea-adipisicing-sit-consequat-enim-elit-cupidatat-origin.png"
+          />
+          <img className="button-10" alt="Button" src="button-13.svg" />
+          <img className="image-5" alt="Image" src="image-29.svg" />
+        </div>
+        <div className="container-16">
+          <img className="logo-facebook" alt="Logo facebook" src="logo-facebook-1.svg" />
+          <img className="logo-twitter-6" alt="Logo twitter" src="logo-twitter-5.svg" />
+          <div className="textbox">
+            <div className="text-wrapper-29">English</div>
+            <img className="chevron-down-large-2" alt="Chevron down large" src="image.svg" />
+          </div>
+          <div className="text-wrapper-30">Product</div>
+          <div className="text-wrapper-31">Resources</div>
+          <div className="text-wrapper-32">Company</div>
+          <div className="text-wrapper-33">About</div>
+          <div className="text-wrapper-34">Join us</div>
+          <div className="text-wrapper-35">Pricing</div>
+          <div className="text-wrapper-36">Features</div>
+          <div className="text-wrapper-37">Blog</div>
+          <div className="text-wrapper-38">User guides</div>
+          <div className="text-wrapper-39">Webinars</div>
+          <div className="text-wrapper-40">Sitemap</div>
+          <div className="text-wrapper-41">•</div>
+          <div className="text-wrapper-42">© 2022 Brand, Inc.</div>
+          <div className="text-wrapper-43">Privacy</div>
+          <div className="text-wrapper-44">Terms</div>
+          <div className="text-wrapper-45">•</div>
+          <div className="text-wrapper-46">•</div>
+          <img className="logo-youtube" alt="Logo youtube" src="logo-youtube-1.svg" />
+          <img className="logo-linkedin-5" alt="Logo linkedin" src="logo-linkedin-5.svg" />
+          <div className="textbox-2">
+            <div className="text-wrapper-47">Input your email</div>
+            <img className="mail" alt="Mail" src="mail.svg" />
+          </div>
+          <div className="text-wrapper-48">Subscribe to our newsletter</div>
+          <p className="text-wrapper-49">For product announcements and exclusive insights</p>
+          <button className="button-11">
+            <div className="text-wrapper-50">Subscribe</div>
+          </button>
+          <img className="scholarship-3" alt="Scholarship" src="scholarship.png" />
+          <div className="text-wrapper-51">Scholar Quest</div>
+        </div>
+      </div>
+    </div>
+  );
+};
